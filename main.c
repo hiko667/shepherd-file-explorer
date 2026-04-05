a18167b3287a546261b34fb7980d488c9ca742ce
hiko667 <91916@student.pb.edu.pl>
2026-04-05 21:11:19 +0200

    added src files

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "system.h"
+
 int main(int argc, char * argv[])
 {
-    printf("%s", PLATFORM_NAME);
+    printf("%s", SYSTEM_NAME);
 }
---
+++ menu.c
@@ -0,0 +1,7 @@
+#include "menu.h"
+
+int runterminalMenu(char ** options, int count){
+    for(int i = 0; i++; i<count){
+        
+    }
+}
