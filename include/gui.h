047ce5b804ba7f87611a5ecab8586a9eda64bff1
hiko667 <91916@student.pb.edu.pl>
2026-04-23 18:43:34 +0200

    Window

---
+++ include/gui.h
@@ -0,0 +1,5 @@
+#ifndef GUI_RUNTIME_H
+#define GUI_RUNTIME_H
+void runGui(int argc, char **argv);
+
+#endif
--- src/common/main.c
+++ src/common/main.c
@@ -1,8 +1,8 @@
-#include "runtime.h"
+#include "gui.h"
 
 int main(int argc, char * argv[])
 {
-    run();
+    runGui(argc, argv);
     return 0;
 }
 
